045ccce20b1a3d88709277ca82111129e3cd7b6f
Sandip Sapkota <sandipsapkota9844@gmail.com>
2024-03-15 13:48:05 +0545

    ✨feature: command registration

---
+++ index.js
@@ -0,0 +1,39 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import { config } from "dotenv";
+config();
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  message.reply({
+    content: `Hi **${message.author.username}** from ${client.user.tag} !🥳`,
+  });
+});
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+  if (interaction.commandName === "avatar") {
+    const avatarUrl = interaction.user.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 4096,
+    });
+    await interaction.reply({
+      content: `Here is your avatar: ${avatarUrl}`,
+    });
+  }
+});
+
+client.login(process.env.TOKEN);
